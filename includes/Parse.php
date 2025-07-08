8bef7e3e617a1f4e8c446384b3f6098d4824023a
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-07-08 15:16:32 -0600

    Some cleanup

--- includes/Parse.php
+++ includes/Parse.php
@@ -713,8 +713,8 @@ class Parse {
 
 		$totalCategories = 0;
 		if ( is_array( $this->parameters->getParameter( 'category' ) ) ) {
-			foreach ( $this->parameters->getParameter( 'category' ) as $comparisonType => $operatorTypes ) {
-				foreach ( $operatorTypes as $operatorType => $categoryGroups ) {
+			foreach ( $this->parameters->getParameter( 'category' ) as $operatorTypes ) {
+				foreach ( $operatorTypes as $categoryGroups ) {
 					foreach ( $categoryGroups as $categories ) {
 						if ( is_array( $categories ) ) {
 							$totalCategories += count( $categories );
@@ -725,8 +725,8 @@ class Parse {
 		}
 
 		if ( is_array( $this->parameters->getParameter( 'notcategory' ) ) ) {
-			foreach ( $this->parameters->getParameter( 'notcategory' ) as $comparisonType => $operatorTypes ) {
-				foreach ( $operatorTypes as $operatorType => $categories ) {
+			foreach ( $this->parameters->getParameter( 'notcategory' ) as $operatorTypes ) {
+				foreach ( $operatorTypes as $categories ) {
 					if ( is_array( $categories ) ) {
 						$totalCategories += count( $categories );
 					}
@@ -1169,8 +1169,7 @@ class Parse {
 		asort( $sortKeys );
 
 		$sortedArticles = [];
-
-		foreach ( $sortKeys as $oldKey => $newKey ) {
+		foreach ( $sortKeys as $oldKey => $_ ) {
 			$sortedArticles[] = $articles[$oldKey];
 		}
 
